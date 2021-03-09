057963543c4f9275419cf43f8aaeb201d05be701
wperu <wperu62@gmail.com>
2021-03-09 15:07:08 +0100

    ft_push fix

--- srcs/push/ft_push.c
+++ srcs/push/ft_push.c
@@ -6,14 +6,26 @@
 /*   By: wperu <wperu@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 16:47:08 by wperu             #+#    #+#             */
-/*   Updated: 2021/03/05 16:06:08 by wperu            ###   ########lyon.fr   */
+/*   Updated: 2021/03/09 13:46:22 by wperu            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-void ft_push(int *a, int *b)
+void ft_push(t_stack *src, t_stack *dst)
 {
-    
+    t_stack *tmp;
 
+    tmp = src;
+    src = src->next;
+    tmp->next = dst;
+    dst = tmp;
+}
+
+void    ft_push_management(int pile, t_stack *a, t_stack *b)
+{
+    if(pile == 1 && a != NULL)
+        ft_push(a,b);
+    if(pile == 2 && b != NULL)
+        ft_push(b,a);
 }
