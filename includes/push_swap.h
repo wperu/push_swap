d449a7154c4a040c228d795d3fe8985eac4938d7
wperu <wperu62@gmail.com>
2021-03-09 15:09:45 +0100

    update .h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: wperu <wperu@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 16:50:42 by wperu             #+#    #+#             */
-/*   Updated: 2021/03/05 16:08:14 by wperu            ###   ########lyon.fr   */
+/*   Updated: 2021/03/09 14:33:20 by wperu            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,27 @@ typedef struct          s_stack
     struct s_stack       *next;
 }                       t_stack;
 
+typedef struct s_com
+{
+	enum
+	{
+		s,
+		p,
+		r,
+		rr
+	}	com;
+	int pile;
+	struct s_com *next;
+}				t_com;
 
-void    ft_push(t_stack *a, t_stack *b);
+void    ft_push(t_stack *src, t_stack *dst);
 void    ft_swap(t_stack *stack);
 void    ft_rotate(t_stack *stack);
 void    ft_reverse_rotate(t_stack *stack);
+void    ft_management_rotate_rev(int pile, t_stack *a, t_stack *b);
+void    ft_management_rotate(int pile, t_stack *a, t_stack *b);
+void    ft_management_swap(int pile, t_stack *a, t_stack *b);
+void	ft_management_push(int pile, t_stack *a, t_stack *b);
+
 
 #endif 
