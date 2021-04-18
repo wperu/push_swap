a5b6a09a00a1b750d0fa812f7f054f7cf5c79934
wperu <wperu62@gmail.com>
2021-04-18 18:16:48 +0200

    add ft_sort_five

--- push_swap/srcs/little_sort/ft_sort_five.c
+++ push_swap/srcs/little_sort/ft_sort_five.c
@@ -1,31 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_push.c                                          :+:      :+:    :+:   */
+/*   ft_sort_five.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wperu <wperu@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/03 16:47:08 by wperu             #+#    #+#             */
-/*   Updated: 2021/04/18 18:12:47 by wperu            ###   ########lyon.fr   */
+/*   Created: 2021/04/18 18:02:23 by wperu             #+#    #+#             */
+/*   Updated: 2021/04/18 18:16:01 by wperu            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/checker.h"
+#include "../../includes/push_swap.h"
 
-void	ft_push(t_stack **src, t_stack **dst)
+int ft_push_start(t_stack **a, t_stack **b)
 {
-	t_stack	*tmp;
-
-	tmp = *src;
-	*src = (*src)->next;
-	tmp->next = *dst;
-	*dst = tmp;
+    ft_push(b,a);
+    ft_push(b,a);
+    ft_putstr_fd("pb\n",1);
+    ft_putstr_fd("pb\n",1);
+    return(2);
 }
 
-void	ft_management_push(int pile, t_stack **a, t_stack **b)
+
+int ft_sort_five(t_stack **a, t_stack **b)
 {
-	if(pile == 2 && *a != NULL)
-		ft_push(a,b);
-	if(pile == 1 && *b != NULL)
-		ft_push(b,a);
+    int i;
+    i = 0;
+    
+    ft_push_start(a,b);
 }
